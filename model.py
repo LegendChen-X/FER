7ee383a7533844aaa118c68a73af0692f0f256c1
Xiang <chenxiang00825@gmail.com>
2021-04-20 03:58:48 -0400

    resnet18

--- model.py
+++ model.py
@@ -40,7 +40,7 @@ class Base(nn.Module):
         acc = accuracy(out, labels)
         return {'val_loss': loss.detach(), 'val_acc': acc}
 
-class ResNet(Base):
+class ResNet09(Base):
     def __init__(self, in_chnls, num_cls):
         super().__init__()
         self.conv1 = conv_block(in_chnls, 64, pool=True)
@@ -120,7 +120,7 @@ class DeviceDataLoader():
         
 def evaluate(model, val_loader):
     model.eval()
-    print(model)
+    #print(model)
     outputs = [model.validating(batch) for batch in val_loader]
     batch_losses = [x['val_loss'] for x in outputs]
     epoch_loss = torch.stack(batch_losses).mean()
@@ -131,6 +131,111 @@ def evaluate(model, val_loader):
 def get_lr(optimizer):
     for param_group in optimizer.param_groups:
         return param_group['lr']
+        
+        
+def conv3x3(in_planes, out_planes, stride=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+    
+class BasicBlock(nn.Module):
+    expansion = 1
+    def __init__(self, in_planes, planes, stride=1, down_sample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(in_planes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.down_sample = down_sample
+        self.stride = stride
+    
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.down_sample is not None:
+            residual = self.down_sample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+        
+    def training_step(self, batch):
+        images, labels = batch
+        out = self(images)
+        loss = F.cross_entropy(out, labels)
+        return loss
+    
+    def validating(self, batch):
+        images, labels = batch
+        out = self(images)
+        loss = F.cross_entropy(out, labels)
+        acc = accuracy(out, labels)
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+
+class ResNet18(Base):
+
+    def __init__(self, block, layers, num_classes=1000):
+        self.in_planes = 64
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False),
+                                   nn.BatchNorm2d(64),
+                                   nn.ReLU(inplace=True))
+        self.max_pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Sequential(nn.Linear(512 * block.expansion, 128),
+                                nn.BatchNorm1d(128),
+                                nn.ReLU(True),
+                                nn.Linear(128, num_classes))
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm1d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        down_sample = None
+        if stride != 1 or self.in_planes != planes * block.expansion:
+            down_sample = nn.Sequential(
+                nn.Conv2d(self.in_planes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = [block(self.in_planes, planes, stride, down_sample)]
+        self.in_planes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.in_planes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.max_pool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.global_avg_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
         
 def fit(epochs, max_lr, model, train_loader, val_loader, weight_decay, grad_clip, opt_func=torch.optim.Adam):
     torch.cuda.empty_cache()
@@ -159,7 +264,7 @@ def fit(epochs, max_lr, model, train_loader, val_loader, weight_decay, grad_clip
     return history
     
     
-def main(type):
+def main(type, batch, epoch):
     data = np.load("./data/data.npz")
     print("Load the data file successfully, start training:")
 
@@ -189,8 +294,8 @@ def main(type):
     train_data = Dataset(train_images, train_labels, train_transform)
     valid_data = Dataset(test_images, test_labels, valid_transform)
     print("Training and validation dataset initialization finished")
-    torch.manual_seed(209)
-    batch_num = 120
+    torch.manual_seed(413)
+    batch_num = batch
     trainDataLoader = DataLoader(train_data, batch_num, shuffle=True, num_workers=4, pin_memory=True)
     validDataLoader = DataLoader(valid_data, batch_num*2, num_workers=4, pin_memory=True)
     print("TrainDataLoader and validDataLoader initialization finished")
@@ -199,8 +304,10 @@ def main(type):
     validDataLoader = DeviceDataLoader(validDataLoader, device)
     if type=="VGG":
         model = to_device(VGG(1, 7), device)
+    else if type=="ResNet09":
+        model = to_device(ResNet09(1, 7), device)
     else:
-        model = to_device(ResNet(1, 7), device)
+        model = to_device(ResNet18(BasicBlock, [2,2,2,2], 7), device)
     print("Get the model type:" , type)
     print("Start model evaluation:")
     evaluate(model, validDataLoader)
@@ -208,13 +315,13 @@ def main(type):
     grad_clip = 0.1
     weight_decay = 1e-4
     print("Start model fitting:")
-    trainLog = fit(30, max_lr, model, trainDataLoader, validDataLoader, weight_decay, grad_clip, torch.optim.Adam)
+    trainLog = fit(epoch, max_lr, model, trainDataLoader, validDataLoader, weight_decay, grad_clip, torch.optim.Adam)
     torch.save(model.state_dict(), type+'.pth')
     plot_losses(trainLog)
     plot_lrs(trainLog)
     
-def testBias(t, name):
-    data = np.load("../data/test.npz")
+def testBias(t):
+    data = np.load("./data/test.npz")
     model = None
     if t=="VGG":
         print("VGG")
@@ -222,7 +329,7 @@ def testBias(t, name):
     else:
         model = ResNet(1, 7)
     
-    model.load_state_dict(torch.load(name + ".pth", map_location=get_default_device()))
+    model.load_state_dict(torch.load(t + ".pth", map_location=get_default_device()))
     model.cuda()
     res = []
 
@@ -251,6 +358,7 @@ def testBias(t, name):
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Model Type")
-    parser.add_argument("--type", help="VGG or ResNet", required=True)
+    parser.add_argument("--type", help="VGG or ResNet09 or ResNet18", required=True)
     args = parser.parse_args()
-    main(args.type)
+    main(args.type, 400, 33)
+    #testBias("ResNet09")
